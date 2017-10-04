a328b4f1cd9ba1d89493e4d2d03a2a0a9ccbb9fa
meongbego <meongbego@gmail.com>
2017-10-05 00:02:31 +0800

    Adding Brain For Terserah | subject dict and Predicate dict

--- app.py
+++ app.py
@@ -1,52 +1,152 @@
-from flask import Flask, render_template,url_for
+from flask import Flask, render_template,url_for,request, redirect
+import re,json
 app = Flask(__name__)
 app.config.from_object('config');
 
 @app.route('/')
 def index():
-   return render_template('depan.html')
-
-@app.route('/say')
-def say():
-   w_dict = {
-           'terserah' : (
-               'kok kamu ga pengertian sih?',
-               'aku ga mau makan disini',
-               'gue ga suka tempat ini, ga peka banget sih',
-               'ga peka'
-           ),
-           'aku ga apa-apa' : (
-               'sakit banget rasanya',
-               'jahat banget',
-               'hibur aku dong',
-               'minta maaf kek'
-           ),
-           'gue jelek banget sih' : (
-               'bilang dong gue cantik',
-           ),
-           'ga usah ga perlu' : (
-               'bantuin kek',
-               'cuek banget',
-               'iya perlu lah',
-               'kamu gak peduli sama aku'
-           ),
-           'lo berubah banget' : (
-               'kangen lo yang dulu'
-           ),
-           'iya aku tau aku yang salah' :(
-               'egois banget jadi cowok'
-           ),
-           'cie selamat ya' : (
-               'enak banget sih lo, coba gue aja'
-           ),
-           'oke, aku yang salah' : (
-               'dasar cowok egois',
-               'ini semua salahmu'
-           ),
-           'aku belum mau pacaran' : (
-               'aku pengen punya pacar, tapi bukan (yang kayak) kamu'
-           ),
+   w_dict ={
+            'terserah' ,
+            'aku ga apa-apa',
+            'gue jelek banget',
+            'ga usah ga perlu',
+            'lo berubah banget',
+            'cie selamat ya',
+            'oke, aku yang salah',
+            'aku belum mau pacaran'
    }
+   return render_template('depan.html', kode = w_dict)
+
+@app.route('/say', methods=['GET', 'POST'])
+def say():
+
+   diction = [{
+   		"kode": "terserah",
+   		"subject": [{
+   			"kata": "pengertian"
+   		}, {
+   			"kata": "makan"
+   		}, {
+   			"kata": "tempat"
+   		}, {
+   			"kata": "peka"
+   		}],
+         "predikat": [{
+   			"kata": "kok kamu ga pengertian"
+   		}, {
+   			"kata": "aku ga mau makan disini"
+   		}, {
+   			"kata": "gue ga suka tempat ini"
+   		}, {
+   			"kata": "ga peka"
+   		}]
+   	},
+   	{
+   		"kode": "aku ga apa-apa",
+   		"subject": [{
+   			"kata": "sakit"
+   		}, {
+   			"kata": "jahat"
+   		}, {
+   			"kata": "hibur"
+   		}, {
+   			"kata": "minta"
+   		}],
+         "predikat": [{
+   			"kata": "sakit banget rasanya"
+   		}, {
+   			"kata": "jahat banget"
+   		}, {
+   			"kata": "hibur aku dong"
+   		}, {
+   			"kata": "minta maaf kek"
+   		}]
+   	},
+   	{
+   		"kode": "gue jelek banget",
+   		"subject": [{
+   			"kata": "cantik"
+   		}],
+         "predikat": [{
+   			"kata": "bilang dong gue cantik"
+   		}]
+   	},
+   	{
+   		"kode": "ga usah ga perlu",
+   		"subject": [{
+   			"kata": "bantu"
+   		}, {
+   			"kata": "cuek"
+   		}, {
+   			"kata": "perlu"
+   		}, {
+   			"kata": "peduli"
+   		}],
+         "predikat": [{
+   			"kata": "bantuin kek"
+   		}, {
+   			"kata": "cuek banget"
+   		}, {
+   			"kata": "iya perlu lah"
+   		}, {
+   			"kata": "kamu gak peduli sama aku"
+   		}]
+   	},
+   	{
+   		"kode": "berubah banget",
+   		"subject": [{
+   			"kata": "kangen"
+   		}],
+         "predikat": [{
+   			"kata": "kangen lo yang dulu"
+   		}]
+   	},
+   	{
+   		"kode": "selamat ya",
+   		"subject": [{
+   			"kata": "enak banget"
+   		}],
+         "predikat": [{
+   			"kata": "enak banget lo, coba gue aja"
+   		}]
+   	},
+   	{
+   		"kode": "aku yang salah",
+   		"subject": [{
+   				"kata": "egois"
+   			}, {
+   				"kata": "salah"
+   			}],
+         "predikat": [{
+   				"kata": "dasar egois"
+   			}, {
+   				"kata": "ini semua salahmu"
+   			}]
+   	},
+   	{
+   		"kode": "belum mau pacaran",
+   		"subject": [{
+   			"kata": "pengen punya pacar"
+   		}],
+         "predikat": [{
+   			"kata": "aku pengen punya pacar, tapi bukan (yang kayak) kamu"
+   		}]
+   	}]
+
+   rule =''
+   if request.method == 'POST':
+      g_code = request.form['g_code']
+      g_say = request.form['g_say']
+      for w_dict in diction:
+         if w_dict['kode'] == g_code:
+            for k_dict in w_dict['subject']:
+               rule = '(\w+) '+k_dict['kata']
+               print rule
+         else:
+            pass
+   else:
+      pass
+      # return redirect(url_for(''))
    return render_template('say.html')
 
 if __name__ == '__main__':
